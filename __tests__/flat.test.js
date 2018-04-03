ea4f0207052d251c2c7494219a15034b1eca7d06
sharkstoned <stanlyraschinkin@gmail.com>
2018-04-04 00:56:20 +0300

    code climate issue

--- __tests__/flat.test.js
+++ __tests__/flat.test.js
@@ -6,15 +6,9 @@ const base = '__tests__/__fixtures__/flat';
 const result = fs.readFileSync(path.resolve(base, 'result'), 'utf8');
 
 test('flat JSON', () => {
-  const before = path.resolve(base, 'json/before.json');
-  const after = path.resolve(base, 'json/after.json');
-
-  expect(genDiff(before, after)).toBe(result);
+  expect(genDiff(`${base}/json/before.json`, `${base}/json/after.json`)).toBe(result);
 });
 
 test('flat YAML', () => {
-  const before = path.resolve(base, 'yaml/before.yml');
-  const after = path.resolve(base, 'yaml/after.yml');
-
-  expect(genDiff(before, after)).toBe(result);
+  expect(genDiff(`${base}/yaml/before.yml`, `${base}/yaml/after.yml`)).toBe(result);
 });
