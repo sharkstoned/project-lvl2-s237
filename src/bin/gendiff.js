2ae6792673d3b4120c076925fc66baae97b06846
sharkstoned <stanlyraschinkin@gmail.com>
2018-04-03 17:54:24 +0300

    returned static definition of version

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,15 +2,12 @@
 
 import program from 'commander';
 import path from 'path';
-import fs from 'fs';
 import genDiff from '..';
 
 const makeAbsolutePath = relative => path.resolve(process.cwd(), relative);
 
-const pkg = JSON.parse(fs.readFileSync('../../package.json', 'utf8'));
-
 program
-  .version(pkg.version)
+  .version('0.2.1')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'Output format');
