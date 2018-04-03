471ebedb3824d04c4f982209bf6391a1225dae1d
sharkstoned <stanlyraschinkin@gmail.com>
2018-04-04 00:30:53 +0300

    added yaml parser

--- src/index.js
+++ src/index.js
@@ -2,47 +2,61 @@ import fs from 'fs';
 import os from 'os';
 import path from 'path';
 import _ from 'lodash';
-// import yaml from 'js-yaml';
+import yaml from 'js-yaml';
 
 const readFromFiles = (...args) => args.map(file => ({
-  ext: path.extname,
+  ext: path.extname(file),
   data: fs.readFileSync(file, 'utf8'),
 }));
 
-// todo:dispatch
-const parseData = inputArr => inputArr.map(str => JSON.parse(str));
+const getParser = (extension) => {
+  switch (extension) {
+    case '.json':
+      return JSON.parse;
+    case '.yml':
+      return yaml.safeLoad;
+    default:
+      return JSON.parse;
+  }
+};
+
+const parseConfigs = inputArr => inputArr.map((config) => {
+  const parse = getParser(config.ext);
+
+  return parse(config.data);
+});
 
 const compare = (data1, data2) => {
   const mergedKeys = [...Object.keys(data1), ...Object.keys(data2)];
   const uniqueKeys = Array.from(new Set(mergedKeys));
 
-  const writeKey = (acc, key) => {
-    const fillTemplate = (sign, dataObj) => `${acc}${os.EOL}  ${sign} ${key}: ${dataObj[key]}`;
+  const makeDiffEntry = (acc, key) => {
+    const makeString = (sign, dataObj) => `${os.EOL}  ${sign} ${key}: ${dataObj[key]}`;
 
     // added
     if (!_.has(data1, key)) {
-      return fillTemplate('+', data2);
+      return `${acc}${makeString('+', data2)}`;
     }
 
     // removed
     if (!_.has(data2, key)) {
-      return fillTemplate('-', data1);
+      return `${acc}${makeString('-', data1)}`;
     }
 
     // was not changed
     if (data1[key] === data2[key]) {
-      return fillTemplate(' ', data1);
+      return `${acc}${makeString(' ', data1)}`;
     }
 
     // changed
-    return `${acc}${os.EOL}  - ${key}: ${data1[key]}${os.EOL}  + ${key}: ${data2[key]}`;
+    return `${acc}${makeString('-', data1)}${makeString('+', data2)}`;
   };
 
-  return `{${uniqueKeys.reduce(writeKey, '')}${os.EOL}}${os.EOL}`;
+  return `{${uniqueKeys.reduce(makeDiffEntry, '')}${os.EOL}}${os.EOL}`;
 };
 
 const genDiff = (path1, path2) => {
-  const [first, second] = parseData(readFromFiles(path1, path2));
+  const [first, second] = parseConfigs(readFromFiles(path1, path2));
 
   return compare(first, second);
 };
