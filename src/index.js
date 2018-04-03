6b9dfd5a8c2b2bbce353115d42bbf3361f3addd0
sharkstoned <stanlyraschinkin@gmail.com>
2018-04-03 17:17:27 +0300

    fixed code climate issues

--- src/index.js
+++ src/index.js
@@ -7,29 +7,32 @@ const readFromFiles = (...args) => args.map(path => fs.readFileSync(path, 'utf8'
 const parseData = inputArr => inputArr.map(str => JSON.parse(str));
 
 const compare = (data1, data2) => {
-  const mergedKeys = Array.from(new Set([...Object.keys(data1), ...Object.keys(data2)]));
+  const mergedKeys = [...Object.keys(data1), ...Object.keys(data2)];
+  const uniqueKeys = Array.from(new Set(mergedKeys));
 
   const writeKey = (acc, key) => {
+    const fillTemplate = (sign, dataObj) => `${acc}${os.EOL}  ${sign} ${key}: ${dataObj[key]}`;
+
     // added
     if (!_.has(data1, key)) {
-      return `${acc}${os.EOL}  + ${key}: ${data2[key]}`;
+      return fillTemplate('+', data2);
     }
 
     // removed
     if (!_.has(data2, key)) {
-      return `${acc}${os.EOL}  - ${key}: ${data1[key]}`;
+      return fillTemplate('-', data1);
     }
 
     // was not changed
     if (data1[key] === data2[key]) {
-      return `${acc}${os.EOL}    ${key}: ${data1[key]}`;
+      return fillTemplate(' ', data1);
     }
 
     // changed
     return `${acc}${os.EOL}  - ${key}: ${data1[key]}${os.EOL}  + ${key}: ${data2[key]}`;
   };
 
-  return `{${mergedKeys.reduce(writeKey, '')}${os.EOL}}${os.EOL}`;
+  return `{${uniqueKeys.reduce(writeKey, '')}${os.EOL}}${os.EOL}`;
 };
 
 const genDiff = (path1, path2) => {
